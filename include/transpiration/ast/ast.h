d9dbc91e0eb2b6adf46ce274835b95345b0d3297
Gabriel Mougard <gabrielm@graphcore.ai>
2022-10-24 14:36:00 +0200

    Building AST: ast.h

---
+++ include/transpiration/ast/ast.h
@@ -0,0 +1,26 @@
+#ifndef AST_AST_H_
+#define AST_AST_H_
+
+#include "abstract_expression.h"
+#include "abstract_node.h"
+#include "abstract_statement.h"
+#include "abstract_target.h"
+#include "assignment.h"
+#include "binary_expression.h"
+#include "block.h"
+#include "call.h"
+#include "expression_list.h"
+#include "for.h"
+#include "function.h"
+#include "function_parameter.h"
+#include "if.h"
+#include "index_access.h"
+#include "literal.h"
+#include "operator_expression.h"
+#include "return.h"
+#include "ternary_operator.h"
+#include "unary_expression.h"
+#include "variable.h"
+#include "variable_declaration.h"
+
+#endif // AST_AST_H_
